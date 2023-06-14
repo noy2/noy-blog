e5dde755d5449442bdc0ddc0952b2950aa818242
noy2 <gameplay3542@icloud.com>
2023-06-15 07:13:19 +0900

    Init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const { withContentlayer } = require("next-contentlayer");
+module.exports = withContentlayer({
+  reactStrictMode: true,
+});
