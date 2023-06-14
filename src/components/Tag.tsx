d7578bc3ee154cb22d935f52d6482083f5f8e667
noy2 <gameplay3542@icloud.com>
2023-06-15 07:14:58 +0900

    Add: components for tag

---
+++ src/components/Tag.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  children: string;
+}
+
+const Tag = ({ children }: Props) => {
+  return (
+    <div className="px-2 py-1 bg-gray-200 text-sm rounded-md">{children}</div>
+  );
+};
+
+export default Tag;
