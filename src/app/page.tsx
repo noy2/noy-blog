76f74e1d1c503bfd75bf09e325428c35c0a7e233
noy2 <gameplay3542@icloud.com>
2023-06-17 04:48:38 +0900

    Modified: added component for post list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Blog from "@/pages/Blog";
+
 export default function Home() {
   return (
     <main>
       <div className="mx-auto px-6 container flex justify-center">
         <div className="w-full max-w-screen-sm flex flex-col space-y-10 md:space-y-16">
-          Hello
+          <Blog />
         </div>
       </div>
     </main>
